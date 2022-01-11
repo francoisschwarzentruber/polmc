de37a843b9faf46640014fb4da0119655baac9e4
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2022-01-11 21:08:51 +0100

    surv

--- python_starfree_existential/polmc.py
+++ python_starfree_existential/polmc.py
@@ -1,53 +1,68 @@
-#pip install pysat
-#pip install automata_toolkit
-#pip install graphviz
-#pip install hashlib
+# pip install pysat
+# pip install automata_toolkit
+# pip install graphviz
+# pip install hashlib
 
 from pysat.solvers import Glucose3
 from automata_toolkit import regex_to_nfa, visual_utils, nfa_to_dfa, dfa_to_efficient_dfa
 import json
 import hashlib
 
+
+from uuid import UUID
+
+
+class UUIDEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, UUID):
+            # if the obj is uuid, we simply return the value of uuid
+            return obj.hex
+        return json.JSONEncoder.default(self, obj)
+
+
+
 # input: regular expression
 # output: a corresponding DFA
 
 
-def regex_to_dfa(e):
-    NFA = regex_to_nfa.regex_to_nfa("(ab)*")
+def autFromReg(e):
+    NFA = regex_to_nfa.regex_to_nfa(e)
     DFA = nfa_to_dfa.nfa_to_dfa(NFA)
-    return dfa_to_efficient_dfa.dfa_to_efficient_dfa(DFA)
+    return DFA  # dfa_to_efficient_dfa.dfa_to_efficient_dfa(DFA)
 
 
-A = regex_to_dfa("(ab)*")
+# to draw the automaton A
+#
 
-#to draw the automaton A
-#visual_utils.draw_dfa(A)
-
-def printAutomaton(A):
-    print("alphabet:")
-    for a in A["alphabets"]:
-            print(a)
-
-    print("states:")
-    for state in A["states"]:
-            print(state)
-    print("")
-    print("transition_function:")
-    for tr in A["transition_function"]:
-        print(tr)
-
-    print("")
-    print(A["states"])
-    print(A["transition_function"])
-    print(A["initial_state"])
-    print(A["reachable_states"])
-
-printAutomaton(A)
+def autInitialState(A):
+    return A["initial_state"]
 
 
+def autGetSuccessor(A, state, letter):
+    if letter in A["transition_function"][state]:
+        return A["transition_function"][state][letter]
+    else:
+        return "phi"
 
-#visual_utils.draw_dfa(A, title="")
-#print(A)
+
+def autIsFinal(A, state):
+    return state in A["final_states"]
+
+
+def autStates(A):
+    return A["states"]
+
+
+def autFinalStates(A):
+    return A["final_states"]
+
+
+def autShow(A):
+    visual_utils.draw_dfa(A, title="")
+
+
+#
+# print(A)
 
 
 class POLModel:
@@ -65,20 +80,23 @@ class POLModel:
 # output: an ID
 
 
-def strToInt(str):
-    return int(hashlib.md5(str.encode('utf-8')).hexdigest(), 16) % 10000
-
 
 class PropDictionnary:
-    objs = {}
+    objs = {}#id to obj
+    dict = {} #str to id
+    nextID = 1
 
     # input: an object
     # register that object
     # output: the id of that object (an integer)
     def id(self, obj):
-        id = strToInt(json.dumps(obj))
-        self.objs[id] = obj
-        return id
+        s = json.dumps(obj,cls=UUIDEncoder)
+        if(not s in self.dict):
+            self.dict[s] = self.nextID
+            self.objs[self.nextID] = obj
+            self.nextID = self.nextID + 1
+            
+        return self.dict[s]
 
     #input: id
     # output: the object corresponding to that id
@@ -90,6 +108,10 @@ class Solver:
     g = Glucose3()
     d = PropDictionnary()
 
+    def addExist(self, PROP):
+        # [2, 5, 6, 9]  at least one is true
+        self.g.add_clause([self.d.id(p) for p in PROP])
+
     # add the constraint that exactly one proposition in PROP is true
     def addExistUnique(self, PROP):
         # [2, 5, 6, 9]  at least one is true
@@ -100,8 +122,8 @@ class Solver:
                     # [-2, -5] both cannot be true at the same time
                     self.g.add_clause([-self.d.id(p), -self.d.id(q)])
 
-
     # we say that that propositin prop
+
     def addProp(self, prop):
         self.g.add_clause([self.d.id(prop)])
 
@@ -109,6 +131,21 @@ class Solver:
     def get_model(self):
         self.g.solve()
         return list(filter(lambda o: o != None, map(lambda id: self.d.getProp(id), self.g.get_model())))
+
+    def addClause(self, clause):
+        self.g.add_clause([self.d.id(p) for p in clause.posProp] +
+                          [-self.d.id(p) for p in clause.negProp])
+
+
+class Clause:
+    posProp = []
+    negProp = []
+
+    def addPos(self, prop):
+        self.posProp.append(prop)
+
+    def addNeg(self, prop):
+        self.negProp.append(prop)
 
 
 M = POLModel()
@@ -123,17 +160,61 @@ phi = ["K", "a", "p"]
 alphabet = ["a", "b"]
 
 solver = Solver()
-solver.addProp({"type": "true", "w": 0, "phi": phi})
-
-# GuessWord
-for t in range(5):
-    solver.addExistUnique([{"type": "p", "t": t, "a": a} for a in alphabet])
 
 # surv
+#input: automaton
+#        idA id of the automaton
+#
 
 
-# rules
+def surv(A, idA, k):
+    solver.addProp({"type": "a", "automaton": idA, "t": 0, "q": autInitialState(A)})
+    
+    for t in range(k+1):
+        solver.addExistUnique([{"type": "a", "automaton": idA, "t": t, "q": q} for q in autStates(A)])
+
+    for t in range(k):
+        for a in alphabet:
+            for q in autStates(A):
+                c = Clause()
+                c.addNeg({"type": "a", "automaton": idA, "t": t, "q": q} )
+                c.addNeg({"type": "p", "t": t, "a": a} )
+                c.addPos({"type": "a", "automaton": idA, "t": t+1, "q": autGetSuccessor(A, q, a)} ) #it is deterministic contrary to the paper
+                solver.addClause(c)
+                # rules
+
+    solver.addExist([{"type": "a", "automaton": idA, "t": k, "q": q} for q in autFinalStates(A) ])
 
 
-print(solver.get_model())
+# def test2():
+#     solver.addProp({"type": "t", "w": 0, "phi": phi})
 
+#     k = 4
+#     # GuessWord
+#     for t in range(k):
+#         solver.addExistUnique([{"type": "p", "t": t, "a": a} for a in alphabet])
+
+#     A = autFromReg("(ab)*")
+#     surv(A, "A", k)
+
+#     print(solver.get_model())
+
+# def test0():
+#     solver.addProp("a")
+#     print(solver.get_model())
+
+
+def test1():
+    k = 2
+    # GuessWord
+    for t in range(k):
+        solver.addExistUnique([{"type": "p", "t": t, "a": a} for a in alphabet])
+
+    A = autFromReg("ab")
+    autShow(A)
+    surv(A, "A", k)
+
+    print(solver.get_model())
+
+
+test1()
