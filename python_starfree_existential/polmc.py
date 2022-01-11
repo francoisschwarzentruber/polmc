7438575c2f1fc376b318f068ede97a6faf5d147f
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2022-01-11 17:30:19 +0100

    fixed id

--- python_starfree_existential/polmc.py
+++ python_starfree_existential/polmc.py
@@ -1,7 +1,16 @@
+#pip install pysat
+#pip install automata_toolkit
+#pip install graphviz
+#pip install hashlib
+
 from pysat.solvers import Glucose3
 from automata_toolkit import regex_to_nfa, visual_utils, nfa_to_dfa, dfa_to_efficient_dfa
 import json
 import hashlib
+
+# input: regular expression
+# output: a corresponding DFA
+
 
 def regex_to_dfa(e):
     NFA = regex_to_nfa.regex_to_nfa("(ab)*")
@@ -10,6 +19,7 @@ def regex_to_dfa(e):
 
 
 A = regex_to_dfa("(ab)*")
+
 #visual_utils.draw_dfa(A, title="")
 print(A)
 
@@ -25,18 +35,27 @@ class POLModel:
     def addEdge(self, w, u, a):
         self.succ[w].append({"succ": u, "agent": a})
 
+# input: a string
+# output: an ID
+
 
 def strToInt(str):
-    return int(hashlib.md5("aze".encode('utf-8')).hexdigest(), 16)%10000
+    return int(hashlib.md5(str.encode('utf-8')).hexdigest(), 16) % 10000
+
 
 class PropDictionnary:
     objs = {}
 
+    # input: an object
+    # register that object
+    # output: the id of that object (an integer)
     def id(self, obj):
         id = strToInt(json.dumps(obj))
         self.objs[id] = obj
         return id
 
+    #input: id
+    # output: the object corresponding to that id
     def getProp(self, id):
         return self.objs.get(id)
 
@@ -45,22 +64,25 @@ class Solver:
     g = Glucose3()
     d = PropDictionnary()
 
-    def addExistUnique(self,PROP):
+    # add the constraint that exactly one proposition in PROP is true
+    def addExistUnique(self, PROP):
+        # [2, 5, 6, 9]  at least one is true
         self.g.add_clause([self.d.id(p) for p in PROP])
         for p in PROP:
             for q in PROP:
-                if(p != q)
-        self.g.add_clause([-self.d.id(p), -self.d.id(q)])
+                if(p != q):
+                    # [-2, -5] both cannot be true at the same time
+                    self.g.add_clause([-self.d.id(p), -self.d.id(q)])
 
+
+    # we say that that propositin prop
     def addProp(self, prop):
         self.g.add_clause([self.d.id(prop)])
 
-
+    # it returns a valuation that satisfies the set of constraints
     def get_model(self):
         self.g.solve()
-        return list(filter(lambda o: o!=None, map(lambda id : self.d.getProp(id), self.g.get_model())))
-
-
+        return list(filter(lambda o: o != None, map(lambda id: self.d.getProp(id), self.g.get_model())))
 
 
 M = POLModel()
@@ -75,28 +97,17 @@ phi = ["K", "a", "p"]
 alphabet = ["a", "b"]
 
 solver = Solver()
-solver.addProp({"type":"t", "w":0, "phi":phi})
+solver.addProp({"type": "true", "w": 0, "phi": phi})
 
-#add GuessWord
+# GuessWord
 for t in range(5):
-    solver.addExistUnique([{"type":"p", "t":t, "a": a} for a in alphabet])
+    solver.addExistUnique([{"type": "p", "t": t, "a": a} for a in alphabet])
+
+# surv
 
 
-
-
-
-
-
-
-
-
-
+# rules
 
 
 print(solver.get_model())
-
-
-
-
-
 
